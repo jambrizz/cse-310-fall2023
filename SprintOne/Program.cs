35dff1ea66d20b10c2194dfd5bde6b7bbcbd63fe
jambrizz <ambrizjovani@gmail.com>
2023-09-22 01:15:29 -0700

    Setting up debugger

---
+++ SprintOne/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace SprintOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
